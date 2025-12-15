47f40c181544caf742532332cc28830f490a48ad
ncrpaai <154041477+ncrpaai@users.noreply.github.com>
2025-12-15 15:18:26 +0700

    Update quize.h
    Added author information and reformatted comments.

--- quize.h
+++ quize.h
@@ -1,16 +1,20 @@
+// Name : Panyaphat Pipatsaraphan
+// ID : 68070503438
+
 #ifndef QUIZ_H
 #define QUIZ_H
 
-/* Start a quiz for the given topic */
+// Start a quiz for the given topic
 int startQuiz(const char *username, const char *topic);
 
-/* Load questions from quiz.csv for specific topic */
+// Load questions from quiz.csv for specific topic
 int loadQuestions(const char *topic, char questions[][200], char answers[][50], int maxQuestions);
 
-/* Save quiz score to scores.csv */
+// Save quiz score to scores.csv 
 void saveScore(const char *username, const char *topic, int score);
 
-/* Get current attempt number for user and topic */
+// Get current attempt number for user and topic
 int getAttemptNumber(const char *username, const char *topic);
+
 
 #endif
