93ed8b59a1bef2c76bbdff4d5c6f92573c4f1ead
ncrpaai <154041477+ncrpaai@users.noreply.github.com>
2025-12-15 15:21:49 +0700

    Update user.h

--- user.h
+++ user.h
@@ -1,19 +1,23 @@
+// Name : Panyaphat Pipatsaraphan
+// ID : 68070503438
+
 #ifndef USER_H
 #define USER_H
 
 #define MAX_USERNAME 50
 #define MAX_PASSWORD 50
 
-/* Register a new user */
+// Register a new user
 int registerUser();
 
-/* Login existing user */
+// Login existing user
 int loginUser(char *username);
 
-/* Check if username already exists */
+// Check if username already exists 
 int usernameExists(const char *username);
 
-/* Validate password for given username */
+// Validate password for given username
 int validatePassword(const char *username, const char *password);
+
 
 #endif
